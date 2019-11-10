b18720a3bd0cb48e0dc87265536c259045b8c830
Leandro <leaqjandro@hotmail.com>
2019-11-10 08:30:18 -0600

    Small Changes

--- wp-content/themes/BIOTA-SITE/index.php
+++ wp-content/themes/BIOTA-SITE/index.php
@@ -83,82 +83,8 @@
 
       <div class="container marketing">
         <!-- START THE FEATURETTES -->
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">Proyecto Biota. <span class="text-muted"> Trabajo Comunal Universitario TC-726</span></h2>
-            <p class="lead">Apoyo a comunidades rurales para comprender su potencial ecoturístico mediante el conocimiento e interpretación de su biodiversidad, cultura y paisaje.</p>
-          </div>
-          <div class="col-md-5">
-            <video class="featurette-image img-fluid mx-auto" autoplay muted controls src="<?php echo get_theme_file_uri('img/biotavideo1.mp4#t=01'); ?>"></video>
-            <!--<img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">-->
-          </div>
-        </div>
+      
 
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Trabajo en las Comunidades <span class="text-muted">Avistamiento de aves.</span></h2>
-            <p class="lead">En el Proyecto Biota buscamos apoyar a miembros de las comunidades rurales, emprendedores del sector turístico, centros de educación primaria y secundaria y asociaciones de desarrollo a comprender el potencial ecoturístico de su comunidad.</p>
-          </div>
-          <div class="col-md-5 order-md-1">
-          <video class="featurette-image img-fluid mx-auto" autoplay muted controls src="<?php echo get_theme_file_uri('img/pajareovideo1.mp4#t=01'); ?>"></video>
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">¿Qué actividades realiza?</span></h2>
-            <p class="lead">
-              -Identificación de comunidades, líderes y contactor clave.<br>
-              -Inventario e identificación de biodiversidad (plantas, invertebrados, insectors) de interés turístico.<br>
-              -Ejecución de encuestas y actividades de rescate cultural.<br>
-              -Ejecución de actividades de educación ambiental.<br>
-              -Diseño y ejecución de talleres ecoturísticos.<br>
-              -Diseño de redes turísticas.<br>
-              -Creación de material audiovisual e informativo.<br>
-              -Creación e instalación de señalética para interpretación ambiental.<br>
-              -Identificación y mapeo de atracciones naturales y culturales.<br>
-            </p>
-          </div>
-          <div class="col-md-5">
-          <video class="featurette-image img-fluid mx-auto" autoplay muted controls src="<?php echo get_theme_file_uri('img/biotavideo2.mp4#t=01'); ?>"></video>
-          </div>
-        </div>
-        <div class="row featurette">
-          <div class="col-md-12">
-            <h2 class="featurette-heading">¿Donde se realiza?</span></h2>
-            <p class="lead">
-              Comunidades rurales del Atlántico. El proyecto comenzará en cantones de Paraíso, Jimenez y sitios aledaños.
-            </p>
-          </div>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <!-- Three columns of text below the carousel -->
-        <div class="row">
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="<?php echo get_theme_file_uri('img/hospedaje.jpg'); ?>" alt="Generic placeholder image" width="140" height="140">
-            <h2>Hospedaje</h2>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro sequi exercitationem accusantium, cum eum veniam cupiditate ullam sapiente nihil, sint nobis aliquam labore autem eligendi totam vero placeat perspiciatis illo. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">Ver lista &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="<?php echo get_theme_file_uri('img/comidas.jpg'); ?>" alt="Generic placeholder image" width="140" height="140">
-            <h2>Comidas</h2>
-            <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo esse illo quos eaque eveniet! Harum doloremque similique a alias dicta, est nihil in veniam ipsa labore perspiciatis beatae neque culpa? </p>
-            <p><a class="btn btn-secondary" href="#" role="button">Ver lista &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="<?php echo get_theme_file_uri('img/turismo.jpg'); ?>" alt="Generic placeholder image" width="140" height="140">
-            <h2>Turismo de Aventura</h2>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia libero accusantium molestias fuga ad deleniti, placeat doloribus, accusamus, eveniet omnis ipsam! Aliquid iure porro accusamus sed ea quasi necessitatibus blanditiis? </p>
-            <p><a class="btn btn-secondary" href="#" role="button">Ver lista &raquo;</a></p>
-          </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
 
 
         <!-- /END THE FEATURETTES -->
@@ -168,8 +94,7 @@
 
       <!-- FOOTER -->
       <footer class="container">
-        <p class="float-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+     
       </footer>
     </main>
     
